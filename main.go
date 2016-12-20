449cd815c13bc80c81310dc5aa8edcdcf53cb09d
Eric Liao <rcliao01@gmail.com>
2016-12-19 22:15:01 -0800

    feat: add code example using flag

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	textPtr := flag.String("text", "", "Text to parse.")
+	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+	uniquePtr := flag.Bool("unique", false, "Measure unique values of a metric")
+	flag.Parse()
+
+	if *textPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
+}
